3fe4482d3010de65f3a7a3ac08a5b1fdb9ccbc87
MONA-SAZZY <47383757+MONA-SAZZY@users.noreply.github.com>
2019-03-08 16:01:28 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+public class vowcon
+{
+    public static void main(String[] args)
+    
+    {
+char ch;
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+switch (ch)
+{
+    case 'a':
+        System.out.println( "  vowel");
+        break;
+    case 'e':
+        System.out.println(" vowel");
+        break;
+    case 'i':
+        System.out.println( " vowel");
+        break;
+    case 'o':
+        System.out.println( "vowel");
+        break;
+    case 'u':
+        System.out.println( " vowel");
+        break;
+        default:
+        System.out.println( "consonant");
+}
+}
+}
