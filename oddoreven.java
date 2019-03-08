dd09ad810946415cb67e7e7918fedef0fc7d6c20
MONA-SAZZY <47383757+MONA-SAZZY@users.noreply.github.com>
2019-03-08 12:22:07 +0530

    Update oddoreven.java

--- oddoreven.java
+++ oddoreven.java
@@ -6,9 +6,14 @@ class EvenorOdd
     {
         Scanner reader = new Scanner(System.in);
         int num = reader.nextInt();
+        if(num>0)
+        {
         if(num % 2 == 0)
             System.out.println( "Even");
         else
             System.out.println( "Odd");
+        }
+      else
+          System.out.println( "Invalid");   
     }
 }
